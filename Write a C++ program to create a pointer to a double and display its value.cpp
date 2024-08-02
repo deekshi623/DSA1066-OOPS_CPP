826a2c75be1037350fd20367041d797329274f21
deekshi623 <122506748+deekshi623@users.noreply.github.com>
2024-08-02 18:56:34 +0530

    Create Write a C++ program to create a pointer to a double and display its value.cpp

---
+++ Write a C++ program to create a pointer to a double and display its value.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+	double num=3.14;
+	double*ptr=&num;
+	cout<<"value of num:"<<num<<endl;
+	cout <<"address of num:"<<ptr<<endl;
+	cout<<"value of *ptr:"<<*ptr<<endl;
+	
+	
+}
