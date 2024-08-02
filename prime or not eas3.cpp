fccd2d1c7aba5c48c49cb2afa81e96096085beea
deekshi623 <122506748+deekshi623@users.noreply.github.com>
2024-08-02 19:01:28 +0530

    Create prime or not eas3.cpp

---
+++ prime or not eas3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+bool isPrime(int num) {
+    if (num <= 1) {
+        return false; 
+    }
+    for (int i = 2; i <= num / 2; ++i) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true; 
+}
+
+int main() {
+    int number;
+
+    std::cout << "Enter an integer: ";
+    std::cin >> number;
+
+    if (isPrime(number)) {
+        std::cout << number << " is a prime number." << std::endl;
+    } else {
+        std::cout << number << " is not a prime number." << std::endl;
+    }
+
+    return 0;
+}
